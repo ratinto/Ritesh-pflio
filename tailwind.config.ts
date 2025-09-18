0639c78d7b01a4783150a5892ff05825d72e9067
Ritesh Kumar <7976ritesh@gmail.com>
2025-09-18 10:59:47 +0530

    Initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,12 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 const config: Config = withMT({
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
   plugins: [],
